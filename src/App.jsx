41bdb63f2c5c1b7d7f5d9ce00a0d4a1269f407af
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-06-08 15:27:48 -0600

    # Move dogs state and use effect to app.jsx # Pass in data through dogs prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import { CreateDogForm } from "./Components/CreateDogForm";
 import { Dogs } from "./Components/Dogs";
 import { Section } from "./Components/Section";
 import "./fonts/RubikBubbles-Regular.ttf";
 
 function App() {
+  const [allDogs, setAllDogs] = useState([]);
+  const [favDogs, setFavDogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/dogs")
+      .then(res => res.json())
+      .then(data => setAllDogs(data));
+  }, [])
+
   return (
     <div className="App">
       <header>
         <h1>pup-e-picker</h1>
       </header>
       <Section label={"Dogs: "}>
-        <Dogs label={"All Dogs"} />
+        <Dogs label={"All Dogs"} dogs={allDogs} />
         {/* <CreateDogForm /> */}
       </Section>
     </div>
